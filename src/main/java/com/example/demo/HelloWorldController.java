632a39e5c54a83f963b3d6b8e70f8d0ae7a084c0
Richard Weber <richard.weber@nitor.fi>
2017-05-30 08:56:18 +0300

    Hostname in response

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.io.BufferedInputStream;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping("/")
-    public String helloWorld(){
-        return "Hello World";
+    public String helloWorld() {
+        return "Hello World, I am: " + getComputerName();
+    }
+
+    private String getComputerName() {
+        BufferedInputStream in = null;
+        try {
+            Runtime run = Runtime.getRuntime();
+            Process proc = run.exec("hostname");
+            in = new BufferedInputStream(proc.getInputStream());
+            String retval = "";
+            while (in.available() > 0) {
+                retval += (char) in.read();
+            }
+            return retval;
+        } catch (Exception e) {
+            return "Unknown host";
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+
     }
 
 }
