5a6d65abb03f649363d7917262d5feb1f8012449
Richard Weber <richard.weber@nitor.fi>
2017-06-05 12:10:31 +0300

    Load all APP_* env variables as props

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 OPTS=""
 
-if [ ! -z $HELLO_NAME ]; then
-    OPTS=" -Dhello.name=$HELLO_NAME"
-fi
+while read -r line; do
+    OPTS="$OPTS -D$line"
+done <<< $(env | grep "APP_" | sed s/_/\./g | sed -r "s/^APP\.(.*)$/\1/g")
 
 java $OPTS -jar /app.jar
