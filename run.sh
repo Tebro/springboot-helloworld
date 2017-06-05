5a14559d78eb2b896def8acf449d4b09deace5b1
Richard Weber <richard.weber@nitor.fi>
2017-06-05 12:31:51 +0300

    log start command

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ while read -r line; do
     OPTS="$OPTS -D$line"
 done <<< $(env | grep "APP_" | sed s/_/\./g | sed -r "s/^APP\.(.*)$/\1/g")
 
-java $OPTS -jar /app.jar
+start_command="java $OPTS -jar /app.jar"
+
+echo "Starting with command: $start_command"
+
+$start_command
