b9c3b05e1c5d6e376260d3a4a92fe9e67041c616
Richard Weber <richard.weber@nitor.fi>
2017-06-05 13:25:46 +0300

    Config tweaks

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ -f /etc/config/application.properties ]; then
-    ln -s /etc/config/application.properties /app/application.properties
+    cp /etc/config/application.properties /app/application.properties
 fi
 
 OPTS=""
 
 while read -r line; do
-    OPTS="$OPTS -D$line"
+    if [ ! -z "${line// }" ]; then
+        OPTS="$OPTS -D$line"
+    fi
 done <<< $(env | grep "APP_" | sed s/_/\./g | sed -r "s/^APP\.(.*)$/\1/g")
 
 start_command="java $OPTS -jar /app/app.jar"
