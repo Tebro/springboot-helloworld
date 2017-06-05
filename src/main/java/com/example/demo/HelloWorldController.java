e41c642500abe4fedc7905db724db94b7fe494b7
Richard Weber <richard.weber@nitor.fi>
2017-06-05 10:21:40 +0300

    Moved 'world' to properties

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 public class HelloWorldController {
 
+    @Value("${hello.name}")
+    private String helloName;
+
     @Autowired
     HostnameFinder hostnameFinder;
 
     @RequestMapping("/")
     public String helloWorld() {
-        return "Hello World, I am: " + hostnameFinder.get();
+        return "Hello" + helloName +" , I am: " + hostnameFinder.get();
     }
 
 }
