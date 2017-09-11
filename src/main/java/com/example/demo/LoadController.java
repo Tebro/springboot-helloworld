13ec0ef3516592c4e7b3b8fb5235af6a40cd433e
Richard Weber <richard.weber@nitor.fi>
2017-09-11 14:52:33 +0300

    imports

--- src/main/java/com/example/demo/LoadController.java
+++ src/main/java/com/example/demo/LoadController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
